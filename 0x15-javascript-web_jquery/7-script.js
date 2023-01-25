b5f8af7be40441def6c29264cd52dcfd84d33df1
PrincessRuth90 <marcusruth86@gmail.com>
2023-01-25 18:03:21 +0000

    Task

--- 0x15-javascript-web_jquery/7-script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (resp, status) {
-    if (status === 'success') {
-      $('#character').text(resp.name);
-    }
-  });
-});
